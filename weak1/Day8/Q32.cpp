43b3303bd249368a0fe3a246f976b115f6ea6e71
Satish <be.alwaysss09@gmail.com>
2026-06-10 08:06:06 +0530

    update

--- weak1/Day8/Q32.cpp
+++ weak1/Day8/Q32.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 int main() {
-    int rows;
-    cout << "Enter number of rows: ";
-    cin >> rows;
+    int n = 5;
 
-    for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= i; j++) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
             cout << i;
         }
         cout << endl;
     }
+
     return 0;
 }
